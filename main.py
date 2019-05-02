26828e4003a34a403c5aad9716ce33aa4d9fabc6
AlexTatarov <tatarovalexandru@gmail.com>
2019-05-02 17:21:25 +0200

    Fixed errors

--- main.py
+++ main.py
@@ -39,10 +39,14 @@ def main():
 	normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
 									 std=[0.229, 0.224, 0.225])
 
-	transform = transforms.Compose([transforms.ToTensor(), normalize])
+	transform = transforms.Compose([
+		transforms.RandomCrop(32, padding=4),
+    	transforms.RandomHorizontalFlip(),
+    	transforms.ToTensor(), 
+    	normalize])
 
 	trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-	trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+	trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 
 	#testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 	#testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
@@ -61,11 +65,11 @@ def main():
 								momentum=momentum,
 								weight_decay=wd)
 	# Train for 20 epochs
-	for j in range(1,20):
-		train(model, trainloader, criterion, optimizer)
+	for epoch in range(1,200):
+		train(model, trainloader, criterion, optimizer, epoch)
 	#test(model, testloader, criterion, optimizer)
 
-def train(model, trainloader, criterion, optimizer):
+def train(model, trainloader, criterion, optimizer, epoch):
 	batch_time = AverageMeter()
 	data_time = AverageMeter()
 	losses = AverageMeter()
@@ -74,6 +78,7 @@ def train(model, trainloader, criterion, optimizer):
 
 	# switch to train mode
 	model.train()
+	print('Model finished training')
 
 	end = time.time()
 	for i, (input, target) in enumerate(trainloader):
@@ -87,12 +92,13 @@ def train(model, trainloader, criterion, optimizer):
 		# compute output
 		output = model(input_var)
 		loss = criterion(output, target_var)
-
+		print(loss.size())
 		# measure accuracy and record loss
 		prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
-		losses.update(loss.data[0], input.size(0))
-		top1.update(prec1[0], input.size(0))
-		top5.update(prec5[0], input.size(0))
+		losses.update(loss.item(), input.size(0))
+		top1.update(prec1.item(), input.size(0))
+		top5.update(prec5.item(), input.size(0))
+		print('Everything updated')
 
 		# compute gradient and do SGD step
 		optimizer.zero_grad()
@@ -102,15 +108,16 @@ def train(model, trainloader, criterion, optimizer):
 		# measure elapsed time
 		batch_time.update(time.time() - end)
 		end = time.time()
-
-		if i % 200 == 1:
+		print(i)
+		print(epoch)
+		if epoch % 2 == 0:
 			print('Epoch: [{0}][{1}/{2}]\t'
 				  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
 				  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
 				  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
 				  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
 				  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-				   epoch, i, len(train_loader), batch_time=batch_time,
+				   epoch, i, len(trainloader), batch_time=batch_time,
 				   data_time=data_time, loss=losses, top1=top1, top5=top5))
 
 	print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'.format(top1=top1, top5=top5))
@@ -156,6 +163,7 @@ def accuracy(output, target, topk=(1,)):
 		correct_k = correct[:k].view(-1).float().sum(0)
 		res.append(correct_k.mul_(100.0 / batch_size))
 	return res
+
 if __name__ == '__main__':
 	main()
 
